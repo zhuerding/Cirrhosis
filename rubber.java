bf70cff814a236a66948d2afa7b346b05e8b0efa
zhuerding <34572124+zhuerding@users.noreply.github.com>
2021-08-15 22:00:34 +0800

    Add files via upload

---
+++ rubber.java
@@ -0,0 +1,10 @@
+package top.zhuerding.item;
+
+import net.minecraft.item.Item;
+import top.zhuerding.mods.group.ModGroup;
+
+public class rubber extends Item {
+    public rubber(){
+        super(new Properties().group(ModGroup.itemGroup));
+    }
+}
